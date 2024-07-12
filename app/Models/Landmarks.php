a9a20a79f1c0b22a9cf9409e6cf3b9082e1c76bb
YoHermit <yohermit00@gmail.com>
2024-07-12 08:32:47 +0800

    Removed unused/not found declaration

--- app/Models/Landmarks.php
+++ app/Models/Landmarks.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Landmarks extends Model
 {
-    use HasFactory;
 
     protected $table = 'resordlmark';
     protected $primaryKey = 'ResOrdID';
